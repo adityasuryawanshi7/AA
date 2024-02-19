b303f51d8d89c2c0bd5a5f31a707fb67e11373f0
ak15 <157221199+adityasuryawanshi7@users.noreply.github.com>
2024-02-19 18:19:56 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,12 @@
+let a = document.getElementById("js1").addEventListener("click",function(){
+    alert("😙yes you are, but ye choose nhi karna tha😏")
+}
+)
+let b = document.getElementById("js2").addEventListener("click",function(){
+    alert("kyu?? hn? try another option")
+}
+)
+document.getElementById("js3").addEventListener("click",function(){
+    window.open("https://adityasuryawanshi7.github.io/aa1/")
+}
+)
